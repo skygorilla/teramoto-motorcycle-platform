81ecabf95cf6b93e416a0f42b587ed8b553805b6
sky gorilla <info.skygorilla@gmail.com>
2025-06-06 20:47:07 +0000

    Error: ./src/i18n.ts:4:1 Export getRequestLocale doesn't exist in target

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,13 @@
 
 import {getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
-import {getRequestLocale} from 'next-intl/server';
  
 // Can be imported from a shared config
 const locales = ['en', 'hr'];
  
-export default getRequestConfig(async () => {
+export default getRequestConfig(async ({locale}) => {
   // The locale is now directly available as an argument to the callback.
   // unstable_setRequestLocale in the layout makes this possible.
-  const locale = await getRequestLocale();
  
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
