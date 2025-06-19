537fc33c04fb07f15da92e9cccdec792088ffd1d
sky gorilla <info.skygorilla@gmail.com>
2025-06-19 14:29:25 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,12 @@
 
-import {getRequestConfig, requestLocale} from 'next-intl/server';
+import {getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
  
 // Can be imported from a shared config
 const locales = ['en', 'hr'];
  
-export default getRequestConfig(async () => {
+export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
-  const locale = await requestLocale();
   if (!locales.includes(locale as any)) notFound();
  
   return {
