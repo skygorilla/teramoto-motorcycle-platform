3e6aa2eb9effcfb8ddcd821481f2e0873f2e1102
sky gorilla <info.skygorilla@gmail.com>
2025-06-19 14:13:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,9 +6,6 @@ import {notFound} from 'next/navigation';
 const locales = ['en', 'hr'];
  
 export default getRequestConfig(async ({locale}) => {
-  // The locale is now directly available as an argument to the callback.
-  // unstable_setRequestLocale in the layout makes this possible.
- 
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
  
