f633476ceea409054f1dc461add5297517fd2642
sky gorilla <info.skygorilla@gmail.com>
2025-06-29 22:08:10 +0000

    there is dead space i have to scrool to see the content see other pages

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -317,10 +317,10 @@ SidebarRail.displayName = "SidebarRail"
 
 const SidebarInset = React.forwardRef<
   HTMLDivElement,
-  React.ComponentProps<"main">
+  React.ComponentProps<"div">
 >(({ className, ...props }, ref) => {
   return (
-    <main
+    <div
       ref={ref}
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background transition-[padding] duration-200 ease-linear",
