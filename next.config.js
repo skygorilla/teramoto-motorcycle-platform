2898ba7e9686726d291bfab4f11add19b475256c
sky gorilla <info.skygorilla@gmail.com>
2025-06-30 05:14:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 
+const withNextIntl = require('next-intl/plugin')(
+  './src/i18n.ts'
+);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -26,4 +30,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
