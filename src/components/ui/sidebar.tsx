cc5cddeb72028a67bf8381c21edec0ab4b038677
sky gorilla <info.skygorilla@gmail.com>
2025-07-03 18:31:33 +0000

    i noticed is not responsive

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -218,7 +218,7 @@ const Sidebar = React.forwardRef<
         ref={ref}
         className={cn(
           "group text-sidebar-foreground",
-          "peer hidden md:block" // Add 'peer' and 'hidden md:block'
+          "peer"
         )}
         data-state={state}
         data-collapsible={state === "collapsed" ? collapsible : ""}
