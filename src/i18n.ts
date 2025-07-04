ba66c439024ed37bd1d8c57217be069f7d01226a
sky gorilla <info.skygorilla@gmail.com>
2025-07-04 10:07:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,9 @@
 
-import {getRequestConfig, unstable_getRequestLocale} from 'next-intl/server';
+import {getRequestConfig} from 'next-intl/server';
  
-export default getRequestConfig(async () => {
+export default getRequestConfig(async ({locale}) => {
   // The middleware is responsible for validating the locale.
   // We can assume that a valid locale is provided here.
-  const locale = await unstable_getRequestLocale();
-  
   return {
     messages: (await import(`./messages/${locale}.json`)).default
   };
