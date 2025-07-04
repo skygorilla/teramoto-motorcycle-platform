be3096bd1b5b2869f706005e98ab96f83916c923
sky gorilla <info.skygorilla@gmail.com>
2025-07-04 09:50:20 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,12 @@
 
 import {getRequestConfig} from 'next-intl/server';
+import {getRequestLocale} from 'next-intl/server';
  
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async () => {
   // The middleware is responsible for validating the locale.
   // We can assume that a valid locale is provided here.
-  // Removing the `notFound()` call from this file prevents
-  // a potential circular dependency during the build process.
+  const locale = await getRequestLocale();
+  
   return {
     messages: (await import(`./messages/${locale}.json`)).default
   };
